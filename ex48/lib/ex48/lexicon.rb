287abd4e15b1671282b63c307453a735353d453b
Matt Yee <matthew.n.yee@gmail.com>
2016-02-18 17:45:49 -0800

    Add Readme

--- ex48/lib/ex48/lexicon.rb
+++ ex48/lib/ex48/lexicon.rb
@@ -14,18 +14,6 @@ class Lexicon
   def self.scan(input_str)
     output = []
     input_str.split.each do |word|
-      output << ['number', word.to_i] if Lexicon.integer?(word)
-      output <<  ['direction', word] if TYPE[0].include?(word)
-      output <<  ['verb', word] if TYPE[1].include?(word)
-      output <<  ['stop', word] if TYPE[2].include?(word)
-      output <<  ['noun', word] if TYPE[3].include?(word)
-    end
-    output
-  end
-
-  def self.scan(input_str)
-    output = []
-    input_str.split.each do |word|
       if Lexicon.integer?(word)
         output << ['number', word.to_i]
       elsif TYPE[0].include?(word)
@@ -40,6 +28,7 @@ class Lexicon
         output << ['error', word]
       end
     end
+
     output
   end
 
